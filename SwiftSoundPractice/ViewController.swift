7f8d9c0ad8552cb2f0557069ff76aef7852a3c53
riku <riku.suny@gmail.com>
2022-08-06 20:37:41 +0900

    Initial Commit

---
+++ SwiftSoundPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftSoundPractice
+//
+//  Created by 黒澤陸 on 2022/08/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
